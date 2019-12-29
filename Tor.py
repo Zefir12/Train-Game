d9ddbc13df7adf118ecf96af1b3a4b3fad763965
Patryk Borowiec <56939183+Zefir12@users.noreply.github.com>
2019-12-30 00:51:50 +0100

    Delete Tor.py

--- Tor.py
+++
@@ -1,9 +0,0 @@
-class tor:
-    def __init__(self,x,y):
-        self.x = x
-        self.y = y
-        self.id = id
-        self.color = [255,0,0]
-    def draw(self):
-        pygame.draw.circle(obraz, self.color, [int(self.x), int(self.y)], int(self.size))
-
